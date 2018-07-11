3107479eb75b8cc331ce875d30e668a23c0ffed8
doldenroller <tom@doldenroller.de>
2018-07-11 17:22:57 +0200

    removed test code

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,13 +5,6 @@
 <h1><?= $page->title()->html() ?></h1>
 <?= $page->text()->kirbytext() ?>
 </article>
-<!-- mit Comment -->
-<!-- und noch abissl was --> 
-<div>
-<p>Noch etwas echten Code, das mag er glaub ich lieber hier</p>
-<p>und weils so schön ist noch mehr</p>
-<p>und weils so schön ist noch mehr lalala</p>
-</div>
 </main>
 <?php snippet('footer') ?>
 <?php snippet('scripts') ?>
