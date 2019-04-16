8b5bf2d221b3ab9f5ad7240db1d5c7f9c3821d6e
Tom <tom@Toms-iMac.local>
2019-04-16 15:45:16 +0200

    moved old

--- site/snippets/image-ratio.php
+++
@@ -1,4 +0,0 @@
-<?php $imgratio = $img->ratio();
-if($imgratio > $minratio && $imgratio <= $maxratio){$imgclass = 'stretch-w';}else{$imgclass = 'stretch-h';};
-?>
-<?= $imgclass ?>
--- site/snippets/scripts.php
+++
@@ -1,5 +0,0 @@
-<?php echo js('assets/js/slick/slick.js') ?>
-<?php echo js('assets/js/main.js') ?>
-
-</body>
-</html>
